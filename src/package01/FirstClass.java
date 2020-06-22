aad4c3b3120c1e50ec5d5c9ce12a674db3b31a2a
“ilhan195” <ilhan195@gmail.com>
2020-06-22 10:54:04 -0400

    Second commit from local repository ARMUT

--- src/package01/FirstClass.java
+++ src/package01/FirstClass.java
@@ -2,7 +2,8 @@ package package01;
 
 public class FirstClass {
 	public static void main(String[] args) {
-	System.out.println("First commit from local repository");
+	System.out.println("First commit from local repository ELMA");
+	System.out.println("Second commit from local repository ARMUT");
 
 }
 }
