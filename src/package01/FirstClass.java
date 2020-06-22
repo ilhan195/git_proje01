d4c7dc907fdf14a813245304dab3fe6d25146576
“ilhan195” <ilhan195@gmail.com>
2020-06-22 11:09:22 -0400

    From Feature Branch First Message

--- src/package01/FirstClass.java
+++ src/package01/FirstClass.java
@@ -4,6 +4,8 @@ public class FirstClass {
 	public static void main(String[] args) {
 	System.out.println("First commit from local repository ELMA");
 	System.out.println("Second commit from local repository ARMUT");
+	System.out.println("New feature  KARPUZ");
+	
 
 }
 }
